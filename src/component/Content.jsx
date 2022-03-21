8a27740c40c7610a8afc7fd0d57040c20e50b4ff
sukabli <roilance7@gmail.com>
2022-03-21 18:03:37 +0800

    initial commit

---
+++ src/component/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Content(){
+    return (
+        <div>
+            <h1>this is the best thing to do</h1>
+        </div>
+    )
+}
+
+
+export default Content;
